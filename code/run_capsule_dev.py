9cb878f73ea0c36cbcc43d1cd7b7574be00379a5
Kenta Hagihara <kenta.hagihara@alleninstitute.org>
2024-12-17 22:07:53 +0000

    Changes in 4 files

--- code/run_capsule_dev.py
+++ code/run_capsule_dev.py
@@ -46,11 +46,11 @@ sessionname = sessionfolder.split('behavior_')[1]
 fibfolder = sessionfolder + '/fib'
 
 mouse_id = sessionname.split('_')[0]
+sessionfoldername = os.path.basename(sessionfolder)
 
-setup_logging("aind-fiber-raw-qc", mouse_id=mouse_id, session_name = sessionname)
+#setup_logging("aind-fiber-raw-qc", mouse_id=mouse_id, session_name = sessionfoldername)
 
 #need to skip the entire QC if FIP files don't exist
-
 try:
     file1  = glob.glob(fibfolder + os.sep + "FIP_DataG*")[0]
     file2 = glob.glob(fibfolder + os.sep + "FIP_DataIso_*")[0]
@@ -76,7 +76,6 @@ with open(file3) as f:
     datatemp = np.array([row for row in reader])
     data3 = datatemp[:,:].astype(np.float32)
     #del datatemp
-
 
 #%% read behavior json file
 behavior_json_path = sessionfolder + '/behavior/behavior_' + sessionname + '.json'
@@ -148,7 +147,7 @@ plt.subplots_adjust(wspace=0.8)
 
 plt.show()
 plt.savefig(str(results_folder) + '/CMOS_Floor.png',dpi=300, bbox_inches='tight')
-plt.savefig('/root/capsule/results/CMOS_FLoor.pdf')
+plt.savefig('/root/capsule/results/CMOS_Floor.pdf')
 
 
 #%% sync pulse diff
@@ -160,6 +159,50 @@ plt.xlabel("ms")
 
 plt.show()
 plt.savefig('/root/capsule/results/SyncPulseDiff.png')
+
+#%%
+import kachery_cloud as kcl
+
+def get_kachery_secrets():
+    """Obtains the three kachery-cloud environment keys/secrets"""
+    secret_name = "/aind/prod/kachery/credentials"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    
+    kachery_secrets = json.loads(secret)
+
+    os.environ['KACHERY_ZONE'] = kachery_secrets['KACHERY_ZONE']
+    os.environ['KACHERY_CLOUD_CLIENT_ID'] = kachery_secrets['KACHERY_CLOUD_CLIENT_ID']
+    os.environ['KACHERY_CLOUD_PRIVATE_KEY'] = kachery_secrets['KACHERY_CLOUD_PRIVATE_KEY']
+
+get_kachery_secrets()
+
+# %%
+file_path0 = "/root/capsule/results/raw_traces.png"
+uri0 = kcl.store_file(file_path0, label=file_path0)
+
+file_path1 = "/root/capsule/results/CMOS_Floor.png"
+uri1 = kcl.store_file(file_path1, label=file_path1)
+
+file_path2 = "/root/capsule/results/SyncPulseDiff.png"
+uri2 = kcl.store_file(file_path2, label=file_path2)
 
 #%%
 Metrics = dict()
@@ -252,8 +295,8 @@ eval0 = QCEvaluation(
     modality=Modality.FIB,
     stage=Stage.RAW,
     metrics=[
-        QCMetric(name="Data length same", value=len(data1), status_history=[Bool2Status(Metrics["IsDataSizeSame"])]),
-        QCMetric(name="Session length >15min", value=len(data1)/20/60, status_history=[Bool2Status(Metrics["IsDataLongerThan15min"])])
+        QCMetric(name="Data length same", value=len(data1), status_history=[Bool2Status(Metrics["IsDataSizeSame"])], reference=uri0),
+        QCMetric(name="Session length >15min", value=len(data1)/20/60, status_history=[Bool2Status(Metrics["IsDataLongerThan15min"])], reference=uri0)
     ],
     description="Pass when GreenCh_data_length==IsoCh_data_length and GreenCh_data_length==RedCh_data_length, and the session is >15min",
 )
@@ -263,8 +306,8 @@ eval1 = QCEvaluation(
     modality=Modality.FIB,
     stage=Stage.RAW,
     metrics=[
-        QCMetric(name="Data length same", value=len(RisingTime), status_history=[Bool2Status(Metrics["IsSyncPulseSame"])]),
-        QCMetric(name="Data length same", value=len(FallingTime), status_history=[Bool2Status(Metrics["IsSyncPulseSameAsData"])]) 
+        QCMetric(name="Data length same", value=len(RisingTime), status_history=[Bool2Status(Metrics["IsSyncPulseSame"])],reference=uri2),
+        QCMetric(name="Data length same", value=len(FallingTime), status_history=[Bool2Status(Metrics["IsSyncPulseSameAsData"])],reference=uri2) 
     ],
     allow_failed_metrics=True, # most incoplete sync pulses can be fixed/recovered during preprocessing (alignment module)
     description="Pass when Sync Pulse number equals data length, and when rising and falling give same lengths",
@@ -288,9 +331,9 @@ eval3 = QCEvaluation(
     modality=Modality.FIB,
     stage=Stage.RAW,
     metrics=[
-        QCMetric(name="Floor average signal in Green channel", value=float(GreenChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Green"])]),
-        QCMetric(name="Floor average signal in Iso channel", value=float(IsoChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Iso"])]),
-        QCMetric(name="Floor average signal in Red channel", value=float(RedChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Red"])])
+        QCMetric(name="Floor average signal in Green channel", value=float(GreenChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Green"])],reference=uri1),
+        QCMetric(name="Floor average signal in Iso channel", value=float(IsoChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Iso"])],reference=uri1),
+        QCMetric(name="Floor average signal in Red channel", value=float(RedChFloorAve), status_history=[Bool2Status(Metrics["CMOSFloorDark_Red"])],reference=uri1)
     ],
     allow_failed_metrics=False,
     description="Pass when CMOS dark floor is <265 in all channel"
@@ -301,7 +344,7 @@ eval4 = QCEvaluation(
     modality=Modality.FIB,
     stage=Stage.RAW,
     metrics=[
-        QCMetric(name="Floor average signal in Green channel", value=float(np.max(np.diff(data1[10:-2,1]))), status_history=[Bool2Status(Metrics["NoSuddenChangeInSignal"])])
+        QCMetric(name="Floor average signal in Green channel", value=float(np.max(np.diff(data1[10:-2,1]))), status_history=[Bool2Status(Metrics["NoSuddenChangeInSignal"])],reference=uri0)
     ],
     allow_failed_metrics=False,
     description="Pass when 1st derivatives of signals are < 5000"
@@ -309,4 +352,68 @@ eval4 = QCEvaluation(
 
 qc = QualityControl(evaluations=[eval0, eval1, eval2, eval3, eval4])
 qc.write_standard_file(output_directory="/results")
+
+
+#%% DocDB
+from aind_data_access_api.document_db import MetadataDbClient
+
+def query_docdb_id(asset_name: str):
+    """
+    Returns docdb_id for asset_name.
+    Returns empty string if asset is not found.
+    """
+
+    # Resolve DocDB id of data asset
+    API_GATEWAY_HOST = "api.allenneuraldynamics.org"
+    DATABASE = "metadata_index"
+    COLLECTION = "data_assets"
+
+    docdb_api_client = MetadataDbClient(
+    host=API_GATEWAY_HOST,
+    database=DATABASE,
+    collection=COLLECTION,
+    )
+
+    response = docdb_api_client.retrieve_docdb_records(
+    filter_query={"name": asset_name},
+    projection={"_id": 1},
+    )
+
+    if len(response) == 0:
+        return ""
+    docdb_id = response[0]["_id"]
+    return docdb_id
+
+# %%
+
+docdb_id = query_docdb_id(os.path.basename(sessionfolder))
+
+
+#%%
+session = boto3.Session()
+credentials = session.get_credentials()
+host = "api.allenneuraldynamics.org"
+
+auth = AWSRequestsAuth(
+aws_access_key=credentials.access_key,
+aws_secret_access_key=credentials.secret_key,
+aws_token=credentials.token,
+aws_host="api.allenneuraldynamics.org",
+aws_region='us-west-2',
+aws_service='execute-api'
+)
+url = f"https://{host}/v1/add_qc_evaluation"
+post_request_content = {"data_asset_id": docdb_id,
+                        "qc_evaluation": qc.model_dump(mode='json')}
+response = requests.post(url=url, auth=auth, 
+                        json=post_request_content)
+
+if response.status_code != 200:
+    print(response.status_code)
+    print(response.text)
+
+
+
+
+
 # %%
